9e334d872b11eb627c0c8806edc766b507547419
unknown <vvilq27@gmail.com>
2022-11-23 23:32:39 +0100

    add source

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "main.h"
 
 //code source : https://learn.microsoft.com/en-us/windows/win32/coreaudio/capturing-a-stream
+//              https://stackoverflow.com/questions/59483071/trying-to-create-a-wav-file-with-capturing-a-stream-from-win32-wasapi-c
 
 int main() {
 
